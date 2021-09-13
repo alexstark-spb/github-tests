25e86d2790d67d7d525a4abe7e890e67fa10507a
Alexander Derevyanko <70107243+alexstark-spb@users.noreply.github.com>
2021-09-13 18:36:10 +0300

    added class Try Drag & Dropp

---
+++ src/test/java/com/github/TryDragAndDrop.java
@@ -0,0 +1,25 @@
+package com.github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TryDragAndDrop {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void dragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-b").shouldHave(Condition.text("A"));
+
+    }
+
+}
